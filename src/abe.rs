1750d961cede3bdf9c0f8e1c20969aeffdba76e4
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-01-07 17:05:50 +0100

    Module-level documentation for `mod abe`

--- src/abe.rs
+++ src/abe.rs
@@ -1,2 +1,16 @@
+//! Attribute based encryption (ABE) schemes.
+//!
+//! CiFEr, the C implementation, supports three different schemes:
+//!
+//! - GPSW, a KP-ABE scheme by [Goyal, Pandey, Sahai, Waters,
+//! 2006](https://eprint.iacr.org/2006/309)
+//! - FAME, a CP-ABE scheme by [Shashank Agrawal and Melissa Chase,
+//! 2017](https://eprint.iacr.org/2017/807).
+//! - DIPPE, a IPPE based scheme by [Yan Michalevsky and Marc Joye,
+//! 2018](https://eprint.iacr.org/2018/753).
+//!
+//! Currently, CiFE-rs, this Rust implementation, aims to fully implement the DIPPE scheme,
+//! and is madel available in the [`dippe`] module.
+
 #[cfg(feature = "dippe")]
 pub mod dippe;
---
+++ src/abe/dippe.rs
@@ -0,0 +1,1 @@
+//! Decentralized inner-product predicate encryption.
