c7050fa7d023beaac11fbaf467b9777cd6640c2d
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-01-11 10:36:48 +0100

    Dippe::encrypt benchmark

--- benches/dippe.rs
+++ benches/dippe.rs
@@ -1,5 +1,6 @@
 use cife_rs::abe::dippe::*;
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use rabe_bn::*;
 
 fn dippe_setup(c: &mut Criterion) {
     let mut group = c.benchmark_group("Dippe::new");
@@ -18,6 +19,32 @@ fn dippe_setup(c: &mut Criterion) {
             let dippe = Dippe::new(&mut rng, k);
             b.iter(move || black_box(dippe.generate_key_pair(&mut rng)));
         });
+    }
+    group.finish();
+
+    let mut group = c.benchmark_group("Dippe::encrypt");
+    for k in [2, 3].iter() {
+        for attributes in (2..7).map(|x| 1 << x) {
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!("k={}, |attr|={}", k, attributes)),
+                k,
+                |b, &k| {
+                    let mut rng = rand::thread_rng();
+                    let dippe = Dippe::new(&mut rng, k);
+                    let msg = Gt::one();
+                    let (public, _private) = dippe.generate_key_pair(&mut rng);
+
+                    let test_pol_vec =
+                        dippe.create_conjunction_policy_vector(&mut rng, attributes, &[0, 1, 3]);
+
+                    let pks: Vec<_> = (0..(attributes + 1)).map(|_| &public).collect();
+
+                    b.iter(move || {
+                        black_box(dippe.encrypt(&mut rng, &test_pol_vec, msg.clone(), &pks))
+                    });
+                },
+            );
+        }
     }
     group.finish();
 }
